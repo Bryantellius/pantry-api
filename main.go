1b8875e1d899be61658bed9bee36a17df19cae67
Ben Bryant <brbryant2639@gmail.com>
2023-10-17 15:56:46 -0500

    cleaned up moved code; added SetupServer

--- main.go
+++ main.go
@@ -1,64 +1,36 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"time"
 )
 
-type item struct {
-	ID             string    `json:"id"`
-	Name           string    `json:"name"`
-	Amount         string    `json:"amount"`
-	IsLow          bool      `json:"isLow"`
-	ExpirationDate time.Time `json:"expirationDate"`
-	PurchaseDate   time.Time `json:"purchaseDate"`
-}
-
-var pantry = []item{
-	{ID: "1", Name: "Milk", Amount: "1 gallon", IsLow: false, ExpirationDate: time.Date(2023, 10, 20, 0, 0, 0, 0, time.UTC), PurchaseDate: time.Date(2023, 10, 12, 0, 0, 0, 0, time.UTC)},
-}
-
 func main() {
-	router := gin.Default() // initialize gin router
+	SetupEnv()
+
+	SetupServer()
+}
+
+// SetupServer starts gin web server with setting router.
+func SetupServer() {
+	mode := getConfig("GIN_MODE")
+
+	gin.SetMode(mode)
+
+	router := gin.New()
 
 	router.GET("/pantry", getItems)
 	router.GET("/pantry/:id", getItemById)
 	router.POST("/item", postItems)
 
-	router.Run("localhost:8080") // starts a http server
-}
+	port := getConfig("PORT")
 
-// getItems responds with a list of all pantry items as JSON
-func getItems(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, pantry)
-}
-
-// postItems creates a new item and responds with the added item as JSON
-func postItems(context *gin.Context) {
-	var newItem item
-
-	err := context.BindJSON(&newItem) // binds the received json to the newItem
-
-	if err != nil {
-		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": err.Error(), "err": err})
-	} else {
-		pantry = append(pantry, newItem)
-
-		context.IndentedJSON(http.StatusCreated, newItem)
-	}
-}
-
-// getItemById responds with the item corresponding to the ID url parameter as JSON
-func getItemById(context *gin.Context) {
-	id := context.Param("id")
-
-	for _, i := range pantry {
-		if i.ID == id {
-			context.IndentedJSON(http.StatusAccepted, i)
-			return
-		}
+	if port == "" {
+		port = "8080"
 	}
 
-	context.IndentedJSON(http.StatusNoContent, gin.H{"message": "Item not found"})
+	if err := router.Run(":" + port); err != nil {
+		log.Panicf("error: %s", err)
+	}
 }

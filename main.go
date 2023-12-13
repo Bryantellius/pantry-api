8db7232027f62759f7f8304cea645c894c5bdc0e
Ben Bryant <brbryant@uabmc.edu>
2023-12-13 15:16:29 -0600

    updated main and setupRouter

--- main.go
+++ main.go
@@ -1,28 +1,14 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
 	SetupEnv()
 
-	SetupServer()
-}
-
-// SetupServer starts gin web server with setting router.
-func SetupServer() {
-	mode := getConfig("GIN_MODE")
-
-	gin.SetMode(mode)
-
-	router := gin.New()
-
-	router.GET("/pantry", getItems)
-	router.GET("/pantry/:id", getItemById)
-	router.POST("/item", postItems)
+	router := setupRouter()
 
 	port := getConfig("PORT")
 
@@ -33,4 +19,24 @@ func SetupServer() {
 	if err := router.Run(":" + port); err != nil {
 		log.Panicf("error: %s", err)
 	}
+}
+
+// setupRouter starts gin web server with setting router.
+func setupRouter() *gin.Engine {
+	mode := getConfig("GIN_MODE")
+
+	gin.SetMode(mode)
+
+	// Initialize gin router
+	router := gin.New()
+
+	// Define routes
+	router.GET("/ping", func(context *gin.Context) {
+		context.String(200, "pong")
+	})
+	router.GET("/pantry", getItems)
+	router.GET("/pantry/:id", getItemById)
+	router.POST("/item", postItems)
+
+	return router
 }
